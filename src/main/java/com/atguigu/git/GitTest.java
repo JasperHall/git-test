64c393e0e54aebff9ffab9aa54703301e4be4d56
Jasper <1533407096@qq.com>
2022-09-07 23:09:09 +0800

    idea second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -3,5 +3,7 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello my first idea Git!!");
+        System.out.println("hello my first idea Git2222!!");
+
     }
 }
