ad959d5b71689e240d7e57749137b556b6ad11a9
Jasper <1533407096@qq.com>
2022-09-08 15:11:08 +0800

    idea master commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -6,7 +6,7 @@ public class GitTest {
         System.out.println("hello my first idea Git2222!!");
         System.out.println("hello my first idea Git3333!!");
         System.out.println("hello my first idea Git4444!!");
-
+        System.out.println("master test!!");
 
     }
 }
